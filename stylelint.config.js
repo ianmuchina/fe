a3319cc2859aded801055c01f5917c46e5a84f23
Ian Muchina <noreply@ianmuchina.com>
2022-03-16 23:24:59 +0300

    move stylelint to specific file

---
+++ stylelint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-prettier"
+    ],
+
+    rules: {
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: [
+                    'tailwind',
+                    'apply',
+                    'variants',
+                    'responsive',
+                    'screen',
+                ],
+            },
+        ],
+        'declaration-block-trailing-semicolon': null,
+        'no-descending-specificity': null,
+    },
+}
