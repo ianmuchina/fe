842240fa9d54203859f48c36ef9a860e8d963eb8
Ian Muchina <noreply@ianmuchina.com>
2022-03-18 02:30:48 +0300

    updates

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,13 +1,16 @@
 const solutions = require("./docs/_data/solutions.cjs");
 
+const x = solutions
+  .filter(s => s.completed)
+  .map(s => `http://localhost/${s.dir}/index.html`)
+  .concat(["index.html"])
+
+console.log(x)
 module.exports = {
   ci: {
     collect: {
       staticDistDir: "./dist",
-      urls: solutions
-        .filter(s => s.completed)
-        .map(s => `${s.dir}/index.html`)
-        .concat(["index.html"]),
+      url: x,
     },
   },
 };
